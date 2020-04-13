e0f16fd20379b1b821deb20ef873e276fbe00dd2
Kiratus <jppemanoel@gmail.com>
2020-04-13 09:26:27 -0300

    1.1
    Adicionado estado e pontuação opcional

--- cpf/nada.js
+++ cpf/nada.js
@@ -1,6 +1,8 @@
 function gerar(){
-    document.querySelector("p#verify").innerHTML = ''
+    document.querySelector("span#result").innerHTML = ''
+    document.querySelector("span#UF").innerHTML = ''
     let campo = document.querySelector("input#CPF")
+    let pontuacao = document.getElementsByName("radPontua")
     let CPF = []
 
     //gera os 9 primeiros digitos aleatoriamente
@@ -38,8 +40,10 @@ function gerar(){
     let fullCPF = ''
     for(let i = 0; i < CPF.length; i++){
         fullCPF += CPF[i].toFixed()
-        if(i == 2 || i == 5) fullCPF += '.'
-        if(i == 8) fullCPF += '-'
+        if(pontuacao[0].checked == true){
+            if(i == 2 || i == 5) fullCPF += '.'
+            if(i == 8) fullCPF += '-'
+        }
     }
     
     //escreve no campo
@@ -48,14 +52,18 @@ function gerar(){
 
 function verificar(){
     let campo = document.querySelector("input#CPF")
-    let mensagem = document.querySelector("p#verify")
-    let CPF = ''
+    let mensagem = document.querySelector("span#result")
+    let CPF = campo.value
+    let valido = false
     let digito1 = 0
     let digito2 = 0
-    CPF = campo.value
     let arrayCPF = CPF.split("")
+    let estado = ''
+    let pontuacao = document.getElementsByName("radPontua")
+    
 
     //erros
+    if(campo.value == "00000000000" || campo.value == "000.000.000-00") return alert('CPF inválido!')
     if(campo.value.length != 11 && campo.value.length != 14) return alert('Por favor, verifique o CPF digitado.')
     if(campo.value.length == 14){
         if(arrayCPF[3] != '.' || arrayCPF[7] != '.' || arrayCPF[11] != '-') return alert('Por favor, verifique o CPF digitado')
@@ -75,8 +83,10 @@ function verificar(){
     campo.value = ''
     for(let i = 0; i < 11; i++){
         campo.value += arrayCPF[i]
-        if(i == 2 || i == 5) campo.value += '.'
-        if(i == 8) campo.value += '-'
+        if(pontuacao[0].checked == true){
+            if(i == 2 || i == 5) campo.value += '.'
+            if(i == 8) campo.value += '-'
+        }
     }
 
     //verifica digito 1
@@ -104,8 +114,27 @@ function verificar(){
     if(digito1 != arrayCPF[9] || digito2 != arrayCPF[10]){
         mensagem.innerHTML = 'CPF INVÁLIDO!'
         mensagem.style.color = 'red'
+        valido = false
+        return
     } else {
         mensagem.innerHTML = 'CPF VÁLIDO!'
         mensagem.style.color = 'green'
+        valido = true
     }
+
+    //estado
+    switch(Number(arrayCPF[8])){
+        case 0: estado = "RS."; break;
+        case 1: estado = "DF, GO, MT, MS ou TO."; break;
+        case 2: estado = "AM, PA, RR, AP, AC ou RO"; break;
+        case 3: estado = "CE, MA ou PI."; break;
+        case 4: estado = "PB, PE, AL ou RN."; break;
+        case 5: estado = "BA ou SE"; break;
+        case 6: estado = "MG"; break;
+        case 7: estado = "RJ ou ES"; break;
+        case 8: estado = "SP"; break;
+        case 9: estado = "PR ou SC"; break;
+        default: estado = "ERRO"; break;
+    }
+    document.querySelector("span#UF").innerHTML = estado
 }
